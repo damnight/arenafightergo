eef97f97eb345fa671f3eff06ae4f6bf46833e31
Lucy Dust <lucrezia.d.ast@protonmail.com>
2025-06-26 12:56:44 +0200

    building basic ecsgo for current example, without adding anything new

--- tile.go
+++ tile.go
@@ -15,22 +15,23 @@
 package main
 
 import (
-	"log"
-
 	"github.com/hajimehoshi/ebiten/v2"
 )
 
-func main() {
-	ebiten.SetWindowTitle("Isometric (Ebitengine Demo)")
-	ebiten.SetWindowSize(640, 480)
-	ebiten.SetWindowResizingMode(ebiten.WindowResizingModeEnabled)
+// Tile represents a space with an x,y coordinate within a Level. Any number of
+// sprites may be added to a Tile.
+type Tile struct {
+	sprites []*ebiten.Image
+}
 
-	g, err := NewGame()
-	if err != nil {
-		log.Fatal(err)
-	}
+// AddSprite adds a sprite to the Tile.
+func (t *Tile) AddSprite(s *ebiten.Image) {
+	t.sprites = append(t.sprites, s)
+}
 
-	if err = ebiten.RunGame(g); err != nil {
-		log.Fatal(err)
+// Draw draws the Tile on the screen using the provided options.
+func (t *Tile) Draw(screen *ebiten.Image, options *ebiten.DrawImageOptions) {
+	for _, s := range t.sprites {
+		screen.DrawImage(s, options)
 	}
 }
